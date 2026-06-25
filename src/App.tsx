54e8aa5afb0790c54ef91bd2d6de95f4af88af28
tony g <tony@codeology.co.nz>
2026-06-25 15:41:34 +1200

    release v1.7.4

--- src/App.tsx
+++ src/App.tsx
@@ -406,7 +406,7 @@ const App: React.FC = () => {
 
                 <footer>
                     <span>Copyright &copy; 2024 Altered States Limited</span>
-                    <span className="footer__version">v1.7.3</span>
+                    <span className="footer__version">v1.7.4</span>
                 </footer>
             </div>
         </Router>
