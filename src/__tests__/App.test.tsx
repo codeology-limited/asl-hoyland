9b4a6d8d1865ff7a332cae5e2447c8fdf8f8b0dd
tony g <tony@codeology.co.nz>
2026-07-15 17:13:22 +1200

    release v1.8.0

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -55,7 +55,7 @@ describe('App', () => {
     await waitFor(() => {
       expect(screen.getByText(/Copyright.*2024.*Altered States Limited/i)).toBeInTheDocument();
       const versionSpan = screen.getByText((content, element) =>
-        element?.classList?.contains('footer__version') && content.includes('v1.7.9')
+        element?.classList?.contains('footer__version') && content.includes('v1.8.0')
       );
       expect(versionSpan).toBeInTheDocument();
     });
